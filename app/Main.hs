532a1cb862d60ff72f2b3898feace4f322f11ab5
zach <taylorzachary8@gmail.com>
2024-02-24 23:03:56 -0800

    Implemented rudamentary Tetris gameplay mechanics

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import System.Random
+import Data.List (transpose)
 
 -- Board grid is 20x10, each cell has a size of 40
 
@@ -9,14 +11,16 @@ data GameState = GameState
   { 
     grid :: [[Bool]],       -- Tettris game grid represented by 2d array of Bools, True if square is occupied, False otherwise
     currPiece :: Piece,
-    pieceDir :: Direction
+    seed :: StdGen,
+    tick :: Float           -- Time measurement for falling piece
   }
 
 data Piece = Piece 
   { 
     shape :: [[Bool]],
     positionX :: Int,
-    positionY :: Int
+    positionY :: Int,
+    pieceDir :: Direction
   }
 
 data Direction = MoveWest | MoveEast | MoveSouth | None
@@ -30,14 +34,22 @@ shapeO = [[True, True], [True, True]]
 shapeS = [[True, False], [True, True], [False, True]]
 shapeT = [[False, True], [True, True], [False, True]]
 shapeZ = [[False, True], [True, True], [True, False]]
+allShapes = [shapeI, shapeJ, shapeL, shapeO, shapeS, shapeT, shapeZ]
 
-initialState:: GameState
-initialState = GameState 
+randomPiece :: StdGen -> (Piece, StdGen)
+randomPiece seed = (Piece { shape = newShape, positionX = 4, positionY = 15, pieceDir = None }, gen)
+   where newShape = allShapes !! rand
+         (rand, gen) = randomR (0,6) seed
+
+initialState :: StdGen -> GameState
+initialState s = GameState 
    { 
     grid = replicate 20 (replicate 10 False), 
-    currPiece = Piece {shape = shapeZ, positionX = 4, positionY = 15},
-    pieceDir = None
+    currPiece = piece,
+    seed = gen,
+    tick = 0
    }
+   where (piece, gen) = randomPiece s
 
 renderCell :: (Int, Int) -> Color -> Picture
 renderCell (x, y) c = translate (fromIntegral (x * 40 - 180)) (fromIntegral (y * 40 - 380))
@@ -55,35 +67,85 @@ render state = pictures $
 
 
 main :: IO ()
-main = play
-  (InWindow "Tetris" (420, 820) (0, 0))
-  white
-  60
-  initialState
-  render
-  handleInput
-  update
+main = do
+   seed <- getStdGen
+   play
+      (InWindow "Tetris" (400, 800) (0, 0))
+      white
+      60
+      (initialState seed)
+      render
+      handleInput
+      update
 
 handleInput :: Event -> GameState -> GameState
-handleInput (EventKey (SpecialKey KeyDown) Down _ _) state  = state -- { pieceDir = MoveSouth }
-handleInput (EventKey (SpecialKey KeyDown) Up _ _) state    = state -- { pieceDir = None }
-handleInput (EventKey (SpecialKey KeyLeft) Down _ _) state  = state -- { pieceDir = MoveWest }
-handleInput (EventKey (SpecialKey KeyLeft) Up _ _) state    = state -- { pieceDir = None }
-handleInput (EventKey (SpecialKey KeyRight) Down _ _) state = state -- { pieceDir = MoveEast }
-handleInput (EventKey (SpecialKey KeyRight) Up _ _) state   = state -- { pieceDir = None }
-handleInput (EventKey (SpecialKey KeySpace) Down _ _) state = state -- { piecePos = dropPiece state }
+handleInput (EventKey (SpecialKey KeyDown) Down _ _) state  = movePiece MoveSouth state -- state { currPiece = (currPiece state) { pieceDir = MoveSouth } }
+handleInput (EventKey (SpecialKey KeyDown) Up _ _) state    = movePiece None state -- state { currPiece = (currPiece state) { pieceDir = None } }
+handleInput (EventKey (SpecialKey KeyLeft) Down _ _) state  = movePiece MoveWest state -- state { currPiece = (currPiece state) { pieceDir = MoveWest } }
+handleInput (EventKey (SpecialKey KeyLeft) Up _ _) state    = movePiece None state -- state { currPiece = (currPiece state) { pieceDir = None } }
+handleInput (EventKey (SpecialKey KeyRight) Down _ _) state = movePiece MoveEast state -- state { currPiece = (currPiece state) { pieceDir = MoveEast } }
+handleInput (EventKey (SpecialKey KeyRight) Up _ _) state   = movePiece None state -- state { currPiece = (currPiece state) { pieceDir = None } }
+handleInput (EventKey (SpecialKey KeyUp) Down _ _) state    = rotatePiece state
+handleInput (EventKey (SpecialKey KeySpace) Down _ _) state = dropPiece state
 handleInput _ state                                         = state
 
--- dropPiece :: GameState -> Point
--- dropPiece state = (x, -340)
---    where (x, _) = piecePos state
+rotatePiece :: GameState -> GameState
+rotatePiece state = state { currPiece = (currPiece state) { shape = (transpose . reverse) (shape (currPiece state)) } }
 
+dropPiece :: GameState -> GameState
+dropPiece state = generateNewPiece state { currPiece = (currPiece state) { positionY = 0 } }
+
+validMove :: Direction -> Int -> Int -> Bool
+-- TODO: account for differe piece shapes, and already placed pieces
+validMove dir x y = 
+   case dir of
+      MoveEast  -> x <= 8
+      MoveWest  -> x >= 0
+      MoveSouth -> y >= 0
+      None      -> True
+
+newPosition :: Direction -> GameState -> (Int, Int)
+newPosition dir state = 
+   let x = positionX (currPiece state)
+       y = positionY (currPiece state)
+   in case dir of
+      MoveEast  -> (x + 1, y)
+      MoveWest  -> (x - 1, y)
+      MoveSouth -> (x, y - 1)
+      None      -> (x, y)
+
+movePiece :: Direction -> GameState -> GameState
+movePiece dir state =
+   if validMove dir newX newY
+   then state { currPiece = (currPiece state) { positionX = newX, positionY = newY } }
+   else state
+   where (newX, newY) = newPosition dir state
+
+occupiedCells :: Piece -> [(Int, Int)]
+occupiedCells piece = [(x + (positionX piece), y + (positionY piece)) | 
+                       (y, row) <- zip [len,(len-1)..] (shape piece), 
+                       (x, occupado) <- zip [0..] row, occupado]
+   where len = length (shape piece) - 1
+
+generateNewPiece :: GameState -> GameState
+-- set values in grid where currPiece is to True
+-- choose next piece to appear and set currPiece to that
+generateNewPiece state = state { grid = newGrid, currPiece = newPiece, seed = newSeed}
+   where
+      newGrid = foldl (\b (x, y) -> replace2D x y True b) (grid state) (occupiedCells (currPiece state))
+      (newPiece, newSeed) = randomPiece (seed state)
+
+-- Function to replace an element at a specific position in a 2D list
+replace2D :: Int -> Int -> a -> [[a]] -> [[a]]
+replace2D x y newElem xs =
+    take y xs ++ [take x (xs !! y) ++ [newElem] ++ drop (x + 1) (xs !! y)] ++ drop (y + 1) xs
+    
 update :: Float -> GameState -> GameState
-update dt state = state
--- update dt state = 
---   let (x, y) = piecePos state
---   in case pieceDir state of
---      MoveEast  -> state { piecePos = (x + 10 * dt * 6, y) }
---      MoveWest  -> state { piecePos = (x - 10 * dt * 6, y) }
---      MoveSouth -> state { piecePos = (x, y - 10 * dt * 6) }
---      None  -> state
+update dt state =
+   if validMove MoveSouth (positionX (currPiece state)) (positionY (currPiece state) - 1)
+   then if (tick state) >= 1.0
+        then movePiece MoveSouth state { tick = 0 }
+        else state { tick = (tick state) + dt }
+   else if (tick state) >= 1.0 then generateNewPiece state { tick = 0 } else state { tick = (tick state) + dt }
+
+
