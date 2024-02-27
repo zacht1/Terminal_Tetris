70749f2eb670248ed2e7b7d972833cc82968f7ed
zach <taylorzachary8@gmail.com>
2024-02-26 21:21:53 -0800

    Fixed validMove implementation and added constants for game board size

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,14 @@ module Main where
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import System.Random
-import Data.List (transpose)
+import Data.List (transpose, findIndex)
 
 -- Board grid is 20x10, each cell has a size of 40
+-- Constants
+width, height, cellSize :: Int
+width = 10
+height = 20
+cellSize = 40
 
 data GameState = GameState
   { 
@@ -44,7 +49,7 @@ randomPiece seed = (Piece { shape = newShape, positionX = 4, positionY = 15, pie
 initialState :: StdGen -> GameState
 initialState s = GameState 
    { 
-    grid = replicate 20 (replicate 10 False), 
+    grid = replicate height (replicate width False), 
     currPiece = piece,
     seed = gen,
     tick = 0
@@ -52,16 +57,16 @@ initialState s = GameState
    where (piece, gen) = randomPiece s
 
 renderCell :: (Int, Int) -> Color -> Picture
-renderCell (x, y) c = translate (fromIntegral (x * 40 - 180)) (fromIntegral (y * 40 - 380))
+renderCell (x, y) c = translate (fromIntegral (x * cellSize - 180)) (fromIntegral (y * cellSize - 380))
                       $ color c
-                      $ rectangleSolid 40 40
+                      $ rectangleSolid (fromIntegral cellSize) (fromIntegral cellSize)
 
 render :: GameState -> Picture
 -- render state = translate x y $ color blue $ shapeJ 20
 --   where
 --     (x, y) = piecePos state
 render state = pictures $ 
-   [renderCell (x, y) blue | (y, row) <- zip [0..] (grid state), (x, occupado) <- zip [0..] row, occupado] ++
+   [renderCell (x, y) red | (y, row) <- zip [0..] (grid state), (x, occupado) <- zip [0..] row, occupado] ++
    [renderCell (x + (positionX (currPiece state)), y + (positionY (currPiece state))) blue | 
    (y, row) <- zip [0..] (shape (currPiece state)), (x, occupado) <- zip [0..] row, occupado]
 
@@ -89,18 +94,33 @@ handleInput (EventKey (SpecialKey KeyUp) Down _ _) state    = rotatePiece state
 handleInput (EventKey (SpecialKey KeySpace) Down _ _) state = dropPiece state
 handleInput _ state                                         = state
 
+-- TODO: rotate piece does not check that the rotation would cause the piece to move illegally
 rotatePiece :: GameState -> GameState
 rotatePiece state = state { currPiece = (currPiece state) { shape = (transpose . reverse) (shape (currPiece state)) } }
 
+-- TODO: drop piece does not currently account for pieces already placed
 dropPiece :: GameState -> GameState
 dropPiece state = generateNewPiece state { currPiece = (currPiece state) { positionY = 0 } }
-
-validMove :: Direction -> Piece -> [[Bool]] -> Int -> Int -> Bool
-validMove dir piece grid x y = 
-   all (\(newX, newY) -> withinBounds (newX, newY) && not (occupied grid (newX, newY))) $ occupiedCells (movePiece dir piece (x, y))
+   -- where lastY = case findIndex (\x -> x) columnX of
+   --          Just i -> 19 - i
+   --          Nothing -> 0
+   --       columnX = map (\row -> row !! (positionX piece)) (grid state)
+   --       piece = (currPiece state)
 
 --  takes two integer arguments representing the x and y coordinates to be checked and returns a boolean value
-withinBounds (x, y) = x >= 0 && x < width && y >= 0 && y < height
+withinBounds :: (Int, Int) -> Bool
+withinBounds (x, y) = x < width && x >= 0 && y >= 0
+
+unoccupied :: (Int, Int) -> [[Bool]] -> Bool
+unoccupied (x, y) grid = not $ (grid !! y) !! x
+   where len = length grid
+
+validMove :: Direction -> Piece -> GameState -> Bool
+validMove dir piece state = all (\pt -> withinBounds pt && unoccupied pt (grid state)) pieceCells
+   where pieceCells = [(x + pieceX, y + pieceY) | (y, row) <- zip [0..] (shape piece),
+                               (x, occupado) <- zip [0..] row,
+                               occupado]
+         (pieceX, pieceY) = newPosition dir state
 
 newPosition :: Direction -> GameState -> (Int, Int)
 newPosition dir state = 
@@ -117,14 +137,14 @@ piece in currPiece if the move is valid according to the updated validMove funct
 movePiece :: Direction -> GameState -> GameState
 movePiece dir state =
   let (newX, newY) = newPosition dir state
-  in if validMove dir (currPiece state) (grid state) newX newY
+  in if validMove dir (currPiece state) state -- (grid state) newX newY
      then state { currPiece = (currPiece state) { positionX = newX, positionY = newY } }
      else state
 
-
 occupiedCells :: Piece -> [(Int, Int)]
+-- Cells occupied by current piece
 occupiedCells piece = [(x + (positionX piece), y + (positionY piece)) | 
-                       (y, row) <- zip [len,(len-1)..] (shape piece), 
+                       (y, row) <- zip [0..] (shape piece), 
                        (x, occupado) <- zip [0..] row, occupado]
    where len = length (shape piece) - 1
 
@@ -141,13 +161,15 @@ replace2D :: Int -> Int -> a -> [[a]] -> [[a]]
 replace2D x y newElem xs =
     take y xs ++ [take x (xs !! y) ++ [newElem] ++ drop (x + 1) (xs !! y)] ++ drop (y + 1) xs
 
+-- TODO : clear rows when completed
+
 {- updated to pass the current state (including currPiece and grid) to the validMove function when checking for a valid south move. -}
 update :: Float -> GameState -> GameState
 update dt state =
-  if validMove MoveSouth (currPiece state) (grid state) (positionX (currPiece state)) (positionY (currPiece state) - 1)
-    then if (tick state) >= 1.0
-         then movePiece MoveSouth state { tick = 0 }
-         else state { tick = (tick state) + dt }
-    else if (tick state) >= 1.0 then generateNewPiece state { tick = 0 } else state { tick = (tick state) + dt }
+   if validMove MoveSouth (currPiece state) state -- (grid state) (positionX (currPiece state)) (positionY (currPiece state) - 1)
+   then if (tick state) >= 1.0
+        then movePiece MoveSouth state { tick = 0 }
+        else state { tick = (tick state) + dt }
+   else if (tick state) >= 1.0 then generateNewPiece state { tick = 0 } else state { tick = (tick state) + dt }
 
 
